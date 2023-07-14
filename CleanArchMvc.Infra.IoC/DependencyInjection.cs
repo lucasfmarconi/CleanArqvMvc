5fbf43578a6842653614e7fa80eff0ca26264dcb
lucasfmarconi <lucasfmarconi@gmail.com>
2023-07-14 14:19:48 +0200

    Add mediatr as application handler

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,12 +1,13 @@
-﻿using CleanArchMvc.Domain.Interfaces;
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CleanArchMvc.Application.Interfaces;
-using CleanArchMvc.Application.Services;
-using CleanArchMvc.Application.Mappings;
 
 namespace CleanArchMvc.Infra.IoC
 {
@@ -22,11 +23,16 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
-//TODO: Should be there? I think this service registrations should reside somewhere else
+            //TODO: Should be there? I think this service registrations should reside somewhere else
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
 
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(ProductCommand).Assembly);
+            });
 
             return services;
         }
