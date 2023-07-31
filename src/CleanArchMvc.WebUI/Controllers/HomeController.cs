9f22cc503a43aea5cb719088154a4d4782b8ffd4
lucasfmarconi <lucasfmarconi@gmail.com>
2023-07-31 15:18:28 +0200

    Add final changes to setup mvc project as an example

--- src/CleanArchMvc.WebUI/Controllers/HomeController.cs
+++ src/CleanArchMvc.WebUI/Controllers/HomeController.cs
@@ -1,19 +1,10 @@
-﻿using CleanArchMvc.WebUI.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
 
 namespace CleanArchMvc.WebUI.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
@@ -22,11 +13,5 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
